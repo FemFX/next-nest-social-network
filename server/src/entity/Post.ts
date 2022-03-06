8f6e33c1fc9954e6883b4a73bef4880df8ad55d5
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2022-03-06 10:26:25 +0300

    Create Post.ts

---
+++ server/src/entity/Post.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, JoinColumn, ManyToOne } from "typeorm";
+import { Initial } from "./initial/Initial";
+import { User } from "./User";
+
+@Entity("posts")
+export class Post extends Initial {
+  @Column({
+    nullable: false,
+  })
+  title: string;
+  @Column({
+    nullable: false,
+  })
+  text: string;
+  @Column({
+    default: 0,
+  })
+  likes: number;
+  @Column({
+    default: 0,
+  })
+  dislikes: number;
+  @Column({
+    default: 0,
+  })
+  views: number;
+  @Column({
+    nullable: false,
+  })
+  image: string;
+  @ManyToOne(() => User, (user) => user.posts)
+  @JoinColumn()
+  user: User;
+}
