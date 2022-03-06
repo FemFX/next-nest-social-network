65c08f833984ddaac46542d50f5d57d344faac25
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2022-03-06 10:26:41 +0300

    Create app.module.ts

---
+++ server/src/app.module.ts
@@ -0,0 +1,42 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { AuthModule } from "./auth/auth.module";
+import { Post } from "./entity/Post";
+import { User } from "./entity/User";
+import { PostModule } from "./post/post.module";
+import { FileModule } from "./file/file.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from "path";
+import { ConfigModule } from "@nestjs/config";
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, "static"),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+    }),
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "reddit",
+      synchronize: true,
+      logging: true,
+      entities: [User, Post],
+      migrations: ["src/migration/**/*.ts"],
+      subscribers: ["src/subscriber/**/*.ts"],
+    }),
+    PostModule,
+    FileModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
