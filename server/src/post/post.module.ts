d54c8a2a29e772173c051ce56c9f2ae6bb3ae377
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2022-03-06 10:26:13 +0300

    Create post.module.ts

---
+++ server/src/post/post.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "src/auth/auth.module";
+import { Post } from "src/entity/Post";
+import { FileModule } from "src/file/file.module";
+import { PostController } from "./post.controller";
+import { PostService } from "./post.service";
+
+@Module({
+  imports: [AuthModule, FileModule, TypeOrmModule.forFeature([Post])],
+  controllers: [PostController],
+  providers: [PostService],
+})
+export class PostModule {}
